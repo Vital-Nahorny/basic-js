b4f905ecf958f8f1f0b326a2ef94224ba2658df4
Vital Nahorny <vital.nagorny@gmail.com>
2020-10-28 19:50:00 +0300

    feat: decided vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,46 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.reverseFlag = isDirect;
+    this.abc = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
+  }
+
+  encrypt(str, key) {
+    const strArr = [...str.toUpperCase()];
+    const keyArr = [...key.toUpperCase()];
+    let pos = 0;
+    let res = strArr.map( item => {
+      if (this.abc.includes(item)) {
+        let index = (this.abc.indexOf(keyArr[pos]) + this.abc.indexOf(item)) % this.abc.length;
+        pos = ++pos % keyArr.length;
+        return this.abc[index];
+      }
+      return item;
+    } );
+    if (this.reverseFlag === true) {
+      return res.join('');
+    }
+    return res.reverse().join('');
+  }
+
+  decrypt(str, key) {
+    const strArr = [...str.toUpperCase()];
+    const keyArr = [...key.toUpperCase()];
+    let pos = 0;
+    let res = strArr.map( item => {
+      if (this.abc.includes(item)) {
+        let index = this.abc.indexOf(item) - this.abc.indexOf(keyArr[pos]);
+        if (index < 0) index += this.abc.length;
+        pos = ++pos % keyArr.length;
+        return this.abc[index];
+      }
+      return item;
+    } );
+    if (this.reverseFlag === true) {
+      return res.join('');
+    }
+    return res.reverse().join('');
   }
 }
 
