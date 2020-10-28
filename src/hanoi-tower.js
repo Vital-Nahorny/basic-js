a4719cebd1ddabb2ca8843db79cddeb476022534
Vital Nahorny <vital.nagorny@gmail.com>
2020-10-28 11:39:18 +0300

    feat: decided hanoi-tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  let arr = {
+    turns: null,
+    seconds: null
+  };
+  arr.turns = Math.pow(2, disksNumber) - 1;
+  arr.seconds = Math.floor((arr.turns / turnsSpeed) * 60 * 60);
+  return arr;
 };
