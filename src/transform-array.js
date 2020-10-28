0324546aae3ded81077196e0f9c73b776362add2
Vital Nahorny <vital.nagorny@gmail.com>
2020-10-28 11:11:29 +0300

    feat: decided transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,38 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (Array.isArray(arr)) {
+    let res = [];
+
+    for (i = 0; i < arr.length; i++) {
+      
+      switch(arr[i]) {
+        case '--discard-prev':
+          if (res.length !== 0 && arr[i - 2] !== '--discard-next') {
+            res.pop();
+          };
+          break;
+        case '--double-prev':
+          if (res.length !== 0 && arr[i - 2] !== '--discard-next') {
+            res.push(arr[i - 1]);
+          };
+          break;
+        case '--double-next':
+          if (i !== arr.length - 1) {
+            res.push(arr[i + 1]);
+          };
+          break;
+        case '--discard-next':
+          if (i !== arr.length - 1) {
+            i++;
+          };
+          break;
+        default:
+          res.push(arr[i]);
+      };
+    };
+    return res;
+  } else {
+    throw new Error();
+  }
 };
